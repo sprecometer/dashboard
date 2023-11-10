dbbf39d920e5f7543cad4874806ee6a2a1e1529c
Luciana Castro <lucianacastro@MacBook-Air-de-Luciana-2.local>
2023-11-10 23:03:03 +0100

    feat: adding Table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import styles from './page.module.css'
+import Table from './components/Table'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <Table></Table>
     </main>
   )
 }
