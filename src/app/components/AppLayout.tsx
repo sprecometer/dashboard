db6ec7cf2659c55ff7acac9eafeec3d4505ef4ea
Luciana Castro <lucianacastro@MacBook-Air-de-Luciana-2.local>
2023-11-11 02:50:24 +0100

    feat: adding header

--- src/app/components/AppLayout.tsx
+++ src/app/components/AppLayout.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import {
-  DesktopOutlined,
-  FileOutlined,
   PieChartOutlined,
-  TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
-import { Layout, Menu, theme } from 'antd'
+import { ConfigProvider, Layout, Menu, theme } from 'antd'
+import Logo from './Logo'
 
 const { Header, Content, Footer, Sider } = Layout
 
@@ -40,6 +38,11 @@ const items: MenuItem[] = [
   ]),
 ]
 
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}))
+
 export default function AppLayout({ children }: any) {
   const [collapsed, setCollapsed] = useState(false)
   const {
@@ -47,21 +50,36 @@ export default function AppLayout({ children }: any) {
   } = theme.useToken()
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <div style={{ width: '100%' }}>
-          <Content style={{ margin: '0 16px' }}>
-            {children}
-          </Content>
-        </div>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: '#20FC8F',
+        },
+      }}
+    >
+      <Layout style={{ minHeight: '100vh', background: 'rgba(52, 57, 91, 0.60)' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} style={{ backgroundColor: '#171123 !important' }} >
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} style={{ backgroundColor: '#171123' }} />
+        </Sider>
+        <Layout>
+          <Header style={{ display: 'flex', alignItems: 'center', height: '100px', backgroundColor: '#171123' }}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ backgroundColor: '#171123' }}>
+              <Logo /><span style={{ fontFamily: 'Electrolize, sans-serif', fontSize: '40px', color: '#20FC8F', marginLeft: '20px' }}>Sprecometer</span>
+              <div style={{ fontFamily: 'Electrolize, sans-serif', fontSize: '20px', color: '#20FC8F', marginLeft: '15px', paddingLeft: '15px', borderLeft: '1px solid #20FC8F' }}>Office Manager</div>
+
+            </Menu>
+          </Header>
+
+          <div style={{ width: '100%' }}>
+            <Content style={{ margin: '0 16px' }}>
+              {children}
+            </Content>
+          </div>
+          <Footer style={{ textAlign: 'center' }}></Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   )
 }
 
